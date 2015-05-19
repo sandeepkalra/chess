99a9c89ae49d7adf6eb0d81b7cad0f10da30a4b1
Deepshikha Kalra <dkalra111@gmail.com>
2015-05-18 22:37:23 -0500

    removing duplicates and kids that are no longer in club

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
+#!/usr/bin/python
 import re
 student_list = dict()
 result_files = {}
 result_lines = {}
-
+duplicate = {}
 sections = [ "k", "1st", "2nd", "3rd", "4th", "5th", "u1600", "u600", "u300", "mon", "tue", "wed", "thu", "fri"];
 result_files["LME"] = "./lme/results.txt"
 result_files["Casis"] = "./casis/results.txt"
 result_files["Patsy"] = "./patsy/results.txt"
 
 class student:
-	first_name = ""
-	middle_name = ""
-	last_name =""
+	first_name = " "
+	middle_name = " "
+	last_name =" "
 	section = "" 
 	lme_result = ""
 	patsy_result = ""
@@ -79,14 +80,20 @@ for k,v in student_list.items():
 				else:
 					print "ERR" + s.patsy_result
 			s.best_val = 0.0
+			full_name  = s.first_name + " " + s.last_name
+			if duplicate.get(full_name,0)==0:
+				duplicate[full_name]=s.section
+			else:
+				print "Duplicate: ", full_name, " in section : ", duplicate[full_name] , " and " , s.section
 			if s.lme_val != "":
 				s.best_val = s.lme_val
 			if s.casis_val != "" and s.casis_val > s.best_val:
 				s.best_val = s.casis_val
 			if s.patsy_val != "" and s.patsy_val > s.best_val:
 				s.best_val = s.patsy_val
-			if s.lme_result != "" or s.casis_result != "" or s.patsy_result != "":
+			if not (s.lme_result != "" or s.casis_result != "" or s.patsy_result != ""):
 					#print s.section + ", " + s.first_name + ", " + s.last_name + " BEST = ( " + str(s.best_val) + " , LME( " + str(s.lme_val) + " ) CASIS (" + str(s.casis_val) + " ) PATSY ( " + str(s.patsy_val) + " )."
-					print s.section + ", " + str(s.best_val) + " , " + s.first_name + ", "  + s.last_name
-
+					#print s.section + ", " + str(s.best_val) + " , " + s.first_name + ", "  + s.last_name
+					#print s.first_name + ", "+ s.last_name + ", section: " + s.section
+					pass
 	
